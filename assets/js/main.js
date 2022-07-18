d46e142573b2f8b5cf89ee177a9903e1ec12a865
drg809 <drg809@gmail.com>
2022-07-18 22:19:28 +0200

    console with info

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,28 +2,10 @@ $(function() {
    var data = [
    { 
      action: 'type',
-     strings: ["npm install -g mimik^400"],
-     output: '<span class="gray">+mimik@0.10.2 installed</span><br>&nbsp;',
-     postDelay: 1000
-   },
-   { 
-     action: 'type',
-     strings: ["cd tests^400"],
-     output: ' ',
-     postDelay: 1000
-   },
-   { 
-     action: 'type',
      //clear: true,
-     strings: ['mimik run^400'],
+     strings: ['startdust init^400'],
      output: $('.mimik-run-output').html()
-   },
-   { 
-     action: 'type',
-     strings: ["that was easy!", ''],
-     postDelay: 2000
    }
-   
  ];
    runScripts(data, 0);
  });
